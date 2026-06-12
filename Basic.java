dc74e71fccedc931d4a4e4037067ee42f3f190d9
Raghav Vats <raghavmishra98601@gmail.com>
2026-06-12 12:08:43 +0530

    Java basic

---
+++ Basic.java
@@ -0,0 +1,25 @@
+
+public class Basic {
+    public static void main(String[] args) {
+     // A=65 , a=97 , 0=48
+  
+    // convert char to int *
+     
+    //  char ch = 'A';
+    //  int x = ch;
+    //  System.out.print(x); // implicit type casting
+
+    //  char ch = 'A';
+    //  System.out.print((int)ch); // explicit type casting
+
+    // char ch = 'A';
+    // System.out.print(ch+0);
+
+    // convert int to char *
+
+    int x = 65;
+    char ch = (char)x;
+    System.out.print(ch);
+
+    }
+}
